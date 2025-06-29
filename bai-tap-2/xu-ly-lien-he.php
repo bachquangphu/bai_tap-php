58884e501c1a794589391b3a6e1b93556da492ae
Quang Phu <quangphu1242000@gmail.com>
2025-06-29 15:39:10 +0700

    fix-bai-tap

--- bai-tap-2/xu-ly-lien-he.php
+++ bai-tap-2/xu-ly-lien-he.php
@@ -1,13 +1,13 @@
 <?php
     $errors= [];
-    if(!isset($_POST['ho_ten']) || !isset($_POST['email']) || !isset($_POST['tieu_de']) || !isset($_POST['noi_dung']))
+    if (!isset($_POST['ho_ten']) || !isset($_POST['email']) || !isset($_POST['tieu_de']) || !isset($_POST['noi_dung']))
     {  $errors[] =" dữ liệu không có vui lòng kiểm tra lại";
-    }else{
+    } else {
         //kiểm tra họ tên
         $kt_ho_ten = trim($_POST['ho_ten']);
         if ($kt_ho_ten ===""){
             $errors[] ="Họ tên không được để trống" ;
-        }else{
+        } else {
             if (strlen($kt_ho_ten) >= 50){
             $errors[]="Họ Tên quá dài vui lòng kiểm tra lại"; 
             }
@@ -16,7 +16,7 @@
         $kt_email = trim($_POST['email']);
         if (empty($kt_email)){
             $errors[] = "email không được để trống";
-        }else{
+        } else {
             if (!filter_var($kt_email, FILTER_VALIDATE_EMAIL)){
                 $errors[] = "email không hợp lệ vui lòng kiểm tra lại";
             }
@@ -34,7 +34,7 @@
         $kt_noi_dung = trim($_POST['noi_dung']);
         if (empty($kt_noi_dung)){
             $errors[] = "nội dung không được để trống vui lòng kiểm tra lại"; 
-        }else{
+        } else {
             $ngat_dong = nl2br($kt_noi_dung);
         }
         if (empty($errors)){
@@ -43,7 +43,7 @@
             echo "<br>Email: " . $kt_email;
             echo "<br>Tiêu đề: " . $kt_tieu_de;
             echo "<br>Nội dung: " . htmlspecialchars($ngat_dong);
-        }else{
+        } else {
             foreach( $errors as $value) {
                 echo   $value . "<br>" ;
             }
